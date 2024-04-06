b18d1d46ded2c24a92b12960942c2f55bc6fa48b
nikhil94058 <kumarnikhil94058@gmail.com>
2024-04-06 19:18:46 +0530

    bug fixed

---
+++ client/src/components/Card.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div>
+      <div className="w-[332.93px] h-[492.45px] bg-zinc-300 rounded-[15px]" >
+
+      </div>
+    </div>
+  )
+}
+
+export default Card
