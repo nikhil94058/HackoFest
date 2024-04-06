a8ffb2b6062c3cd1f453517ca07ff5b4706fe6dd
AlakhMathur <130376769+AlakhMathur@users.noreply.github.com>
2024-04-07 00:54:11 +0530

    cnflict resolved

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,13 @@
-/*import logo from './logo.svg';
+import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
-import Hero from './components/Hero';*/
-import MyComponent from './components/sidebar';
+import Hero from './components/Hero';
+import HomePage from './Pages/HomePage';
 
 function App() {
   return (
     <div>
-      {/*<Navbar />
-      <Hero />*/
-      /*<HomePage />*/}
-  <MyComponent/>
+      <HomePage />
     </div>
   );
 }
