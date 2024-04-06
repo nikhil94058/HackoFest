e106409f59fed7ea5590e20cd375ff73bb794304
nikhil94058 <kumarnikhil94058@gmail.com>
2024-04-07 03:54:07 +0530

    PropertyPage Added

--- client/src/App.js
+++ client/src/App.js
@@ -4,11 +4,12 @@ import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import HomePage from './Pages/HomePage';
 import LoginPage from './Pages/LoginPage';
+import PropertyPage from './Pages/PropertyPage';
 
 function App() {
   return (
     <div>
-      <LoginPage />
+      <PropertyPage />
     </div>
   );
 }
