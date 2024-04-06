dd621afeaa68afd1f835e39ae7197b3dbebdd3fd
AlakhMathur <130376769+AlakhMathur@users.noreply.github.com>
2024-04-07 01:40:21 +0530

    trends side box added

---
+++ client/src/components/trends.js
@@ -0,0 +1,146 @@
+import * as React from "react";
+import 'tailwindcss/tailwind.css';
+
+function MyComponent() {
+  return (
+    <div className="flex flex-col py-3.5 rounded-xl bg-slate-50 max-w-[280px] text-zinc-500">
+      <div className="flex gap-5 items-start self-center px-5 py-px w-full text-xl font-black text-black max-w-[280px]">
+        <div className="flex-auto mt-3">Trending Localities</div>
+        <img
+          loading="lazy"
+          src="https://cdn.builder.io/api/v1/image/assets/TEMP/cc2f82c15dd0349654a71f9954e04c018d3e61cc7cc9d541ae9c0d9802ffdab6?"
+          className="shrink-0 w-6 aspect-square"
+        />
+      </div>
+      <div className="flex flex-col px-4 mt-5 w-full">
+        <div className="flex gap-5 justify-between w-full text-xs">
+          <div className="flex gap-0 self-start">
+            <div>1.</div>
+            <div> Trending</div>
+          </div>
+          <img
+            loading="lazy"
+            src="https://cdn.builder.io/api/v1/image/assets/TEMP/75578caf947ac30a62ee457d4dad39c950ea31bdde7d70f5e520bb7c992f42f4?"
+            className="shrink-0 w-4 aspect-square"
+          />
+        </div>
+        <div className="mt-1.5 text-base font-bold text-zinc-800">
+          #DLF CYBERCITY
+        </div>
+        <div className="flex gap-0 mt-2 text-sm">
+          <div>456K</div>
+          <div> Tweets</div>
+        </div>
+      </div>
+      <div className="mt-4 w-full border border-solid bg-black bg-opacity-10 border-black border-opacity-10 min-h-[1px]" />
+      <div className="flex flex-col px-4 mt-2.5 w-full">
+        <div className="flex gap-5 justify-between w-full text-xs">
+          <div className="flex gap-0 self-start">
+            <div>2.</div>
+            <div> Trending</div>
+          </div>
+          <img
+            loading="lazy"
+            src="https://cdn.builder.io/api/v1/image/assets/TEMP/75578caf947ac30a62ee457d4dad39c950ea31bdde7d70f5e520bb7c992f42f4?"
+            className="shrink-0 w-4 aspect-square"
+          />
+        </div>
+        <div className="mt-1.5 text-base font-bold text-zinc-800">
+          #DWARKA EXPRESSWAY
+        </div>
+        <div className="flex gap-0 mt-2 text-sm">
+          <div>456K</div>
+          <div> Tweets</div>
+        </div>
+      </div>
+      <div className="mt-4 w-full border border-solid bg-black bg-opacity-10 border-black border-opacity-10 min-h-[1px]" />
+      <div className="flex flex-col px-4 mt-2.5 w-full">
+        <div className="flex gap-5 justify-between w-full text-xs">
+          <div className="flex gap-0 self-start">
+            <div>3.</div>
+            <div> Trending</div>
+          </div>
+          <img
+            loading="lazy"
+            src="https://cdn.builder.io/api/v1/image/assets/TEMP/75578caf947ac30a62ee457d4dad39c950ea31bdde7d70f5e520bb7c992f42f4?"
+            className="shrink-0 w-4 aspect-square"
+          />
+        </div>
+        <div className="mt-1.5 text-base font-bold text-zinc-800">
+          #KMP EXPRESSWAY
+        </div>
+        <div className="flex gap-0 mt-2 text-sm">
+          <div>456K</div>
+          <div> Tweets</div>
+        </div>
+      </div>
+      <div className="mt-4 w-full border border-solid bg-black bg-opacity-10 border-black border-opacity-10 min-h-[1px]" />
+      <div className="flex flex-col px-4 mt-2.5 w-full">
+        <div className="flex gap-5 justify-between w-full text-xs">
+          <div className="flex gap-0 self-start">
+            <div>4.</div>
+            <div> Trending</div>
+          </div>
+          <img
+            loading="lazy"
+            src="https://cdn.builder.io/api/v1/image/assets/TEMP/75578caf947ac30a62ee457d4dad39c950ea31bdde7d70f5e520bb7c992f42f4?"
+            className="shrink-0 w-4 aspect-square"
+          />
+        </div>
+        <div className="mt-1.5 text-base font-bold text-zinc-800">
+          #JEVAR AIRPORT
+        </div>
+        <div className="flex gap-0 mt-2 text-sm">
+          <div>456K</div>
+          <div> Tweets</div>
+        </div>
+      </div>
+      <div className="mt-4 w-full border border-solid bg-black bg-opacity-10 border-black border-opacity-10 min-h-[1px]" />
+      <div className="flex flex-col px-4 mt-2.5 w-full">
+        <div className="flex gap-5 justify-between w-full text-xs">
+          <div className="flex gap-0 self-start">
+            <div>5.</div>
+            <div> Trending</div>
+          </div>
+          <img
+            loading="lazy"
+            src="https://cdn.builder.io/api/v1/image/assets/TEMP/75578caf947ac30a62ee457d4dad39c950ea31bdde7d70f5e520bb7c992f42f4?"
+            className="shrink-0 w-4 aspect-square"
+          />
+        </div>
+        <div className="mt-1.5 text-base font-bold text-zinc-800">#AYODHYA</div>
+        <div className="flex gap-0 mt-2 text-sm">
+          <div>456K</div>
+          <div> Tweets</div>
+        </div>
+      </div>
+      <div className="mt-4 w-full border border-solid bg-black bg-opacity-10 border-black border-opacity-10 min-h-[1px]" />
+      <div className="flex flex-col px-4 mt-2.5 w-full">
+        <div className="flex gap-5 justify-between w-full text-xs">
+          <div className="flex gap-0 self-start">
+            <div>6.</div>
+            <div> Trending</div>
+          </div>
+          <img
+            loading="lazy"
+            src="https://cdn.builder.io/api/v1/image/assets/TEMP/75578caf947ac30a62ee457d4dad39c950ea31bdde7d70f5e520bb7c992f42f4?"
+            className="shrink-0 w-4 aspect-square"
+          />
+        </div>
+        <div className="mt-1.5 text-base font-bold text-zinc-800">
+          #MUMBAI EXPRESSWAY
+        </div>
+        <div className="flex gap-0 mt-2 text-sm">
+          <div>456K</div>
+          <div> Tweets</div>
+        </div>
+      </div>
+      <div className="mt-4 w-full border border-solid bg-black bg-opacity-10 border-black border-opacity-10 min-h-[1px]" />
+      <div className="self-center mt-3 text-sm font-medium text-sky-500">
+        Show more
+      </div>
+    </div>
+  );
+}
+
+
